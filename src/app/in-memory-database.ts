bd349787c8d150a3677993ca370bd06a61035ea1
Andre Albuquerque <aalbuquerqu1@lenovo.com>
2019-05-08 17:36:34 -0300

    added category model and in-memory db

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,16 @@
+import { InMemoryDbService } from "angular-in-memory-web-api";
+
+import { Category } from "./pages/categories/shared/category.model";
+export class InMemoryDatabase implements InMemoryDbService{
+    createDb(){
+        const categories: Category[] = [
+            {id: 1, name: "Moradia", description: "Pagamentos de Contas da Casa"},
+            {id: 2, name: "Saúde", description: "Plano de Saúde e Remédios"},
+            {id: 3, name: "Lazer", description: "Cinema, parques, praoa, etc"},
+            {id: 4, name: "Salário", description: "Recebimento de Salário"},
+            {id: 5, name: "Freelas", description: "Trabalhos como Freelancer"}
+        ];
+        return {categories}
+    }
+    
+}
