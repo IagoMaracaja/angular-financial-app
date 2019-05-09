a0b7700ed237a11da3c730f595dc72b27ac1f860
Andre Albuquerque <aalbuquerqu1@lenovo.com>
2019-05-09 15:33:24 -0300

    adding improvement to categoryForm

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -40,6 +40,15 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
     this.setPageTitle();
   }
 
+  submitForm() {
+    this.submittingForm = true;
+    if(this.currentAction == 'new'){
+      this.createCategory();
+    }else{
+      this.updateCategory();
+    }
+  }
+
   // PRIVATE METHODS
 
   private setCurrentAction() {
@@ -81,4 +90,40 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
     }
   }
 
+  private createCategory() {
+    const category: Category =  Object.assign(new Category(), this.categoryForm.value);
+    this.categoryService.create(category).subscribe (
+      category => this.actionsForSuccess(category),
+      error => this.actionsForError(error)
+    )
+  }
+
+  private updateCategory() {
+    const category: Category =  Object.assign(new Category(), this.categoryForm.value);
+    this.categoryService.update(category).subscribe(
+      category => this.actionsForSuccess(category),
+      error => this.actionsForError(error)
+    )
+  }
+
+  private actionsForSuccess(category: Category) {
+    toastr.success('Success');
+
+    // Redirect/Reload component page
+    this.router.navigateByUrl('categories', {skipLocationChange: true}).then(
+      () => this.router.navigate(['categories', category.id, 'edit'])
+    )
+  }
+
+  private actionsForError (error) {
+    toastr.error('An error has occurred while processing your request');
+
+    this.submittingForm = false;
+
+    if (error.status === 422){
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    } else {
+      this.serverErrorMessages = ['Failed to establish communication with server.']
+    }
+  }
 }
