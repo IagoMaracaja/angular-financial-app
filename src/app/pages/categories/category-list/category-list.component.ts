7e761cc7bc02432c0f2c52a99f137a9980390db0
Andre Albuquerque <aalbuquerqu1@lenovo.com>
2019-05-09 11:20:36 -0300

    Connected the category list component with simulated backend

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Category } from "../shared/category.model";
+import { CategoryService } from "../shared/category.service";
+
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit() {
+    this.categoryService.getAll().subscribe(
+      categories => this.categories = categories,
+      error => alert ('Error on get all categories')
+    )
+  }
+
+  deleteCategory(category){
+    const mustDelete = confirm ("Do you really want to delete this item?");
+
+    if (mustDelete){
+      this.categoryService.delete(category.id).subscribe(
+        () => this.categories = this.categories.filter(element => element != category),
+        () => alert("Error when try to delete this category")
+      )
+    }
   }
 
 }
