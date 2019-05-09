2ce52a319436cd2b741ff1da8f7aa5c5739cc295
Andre Albuquerque <aalbuquerqu1@lenovo.com>
2019-05-09 17:12:57 -0300

    first step to implement Entries Module

---
+++ src/app/pages/entries/shared/entry.model.ts
@@ -0,0 +1,23 @@
+import { Category } from "../../categories/shared/category.model";
+
+export class Entry {
+    constructor(
+        public id?: number,
+        public name?: string,
+        public description?: string,
+        public type?: string,
+        public amount?: string,
+        public paid?: boolean,
+        public categoryId?: number,
+        public category?: Category
+    ){}
+
+    static types = {
+        expense: 'Despesa',
+        revenue: 'Receita'
+    }
+
+    get paidText (): String {
+        return this.paid? 'Pago' : 'Pendente';
+    }
+}
